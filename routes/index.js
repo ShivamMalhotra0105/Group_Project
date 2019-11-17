2d65efaf6b3ec6448ac85ff54398560b8bd974aa
Shivam Malhotra <10399sm@gmail.com>
2019-11-17 10:52:10 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Welcome to Usurvey' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {  });
+});
+
+router.get('/privacy', function(req, res, next) {
+  res.render('privacy', {  });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('auth/register', { });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('auth/login', {  });
+});
+
+router.get('/usurvey', function(req, res, next) {
+  res.render('usurvey', { });
+});
+
+
+
+
+module.exports = router;
